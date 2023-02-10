dfa3d3f9a62ee3991a8667d6a8f05084cc9b8222
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2023-02-10 09:34:21 -0500

    Updated nvim tree sitter and blankline config

--- blankline.lua
+++ blankline.lua
@@ -3,23 +3,11 @@ vim.opt.list = true
 vim.opt.listchars:append "space:⋅"
 
 require("indent_blankline").setup {
+     enabled = true,
+     show_current_context = true,
+     char = "|",
+     char_blankline = '┆',
+     use_treesitter = false,
+     context_char = '*',
+     use_treesitter_scope = false,
 }
--- require("indent_blankline").setup {
---     indentLine_enabled = 1,
---     filetype_exclude = {
---       "help",
---       "terminal",
---       "alpha",
---       "packer",
---       "lspinfo",
---       "TelescopePrompt",
---       "TelescopeResults",
---       "mason",
---       "",
---     },
---     buftype_exclude = { "terminal" },
---     show_trailing_blankline_indent = false,
---     show_first_indent_level = false,
---     show_current_context = true,
---     show_current_context_start = true,
--- }
--- treesitter.lua
+++ treesitter.lua
@@ -1,7 +1,10 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "html", "css", "scss", "vue", "javascript", "typescript", "svelte", "json" },
+  ensure_installed = { "html", "css", "scss", "vue", "javascript", "typescript", "tsx", "svelte", "json" },
   highlight = {
     enable = true,
     use_languagetree = true,
   },
+  indent = {
+    enable = false
+  }
 }
