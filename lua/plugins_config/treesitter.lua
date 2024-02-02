f25d78a149ec3010e045c58071f81329aef9970c
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2024-02-02 12:06:50 -0500

    Updated treesitter config

--- lua/plugins_config/treesitter.lua
+++ lua/plugins_config/treesitter.lua
@@ -5,27 +5,25 @@ return {
 	dependencies = {
 		'nvim-treesitter/nvim-treesitter-refactor'
 	},
-	config = function()
-		require('nvim-treesitter.configs').setup{
-  			ensure_installed = { "html", "css", "scss", "vue", "javascript", "typescript", "tsx", "svelte", "json" },
-  			highlight = {
-    				enable = true,
-    				use_languagetree = true,
-  			},
-  			refactor = {
-    				highlight_definitions = {
-      					enable = true,
-      					clear_on_cursor_move = true
-    				},
-    				highlight_current_scope = { enable = true },
-    				smart_rename = {
-      					enable = true,
-      					keymaps = {
-        					smart_rename = "tr",
-      					},
-    				},
-  			},
-
-		}
-	end
+	main = 'nvim-treesitter.configs',
+	opts = {
+  		ensure_installed = { "html", "css", "scss", "vue", "javascript", "typescript", "tsx", "svelte", "json" },
+  		highlight = {
+    			enable = true,
+    			use_languagetree = true,
+  		},
+  		refactor = {
+    			highlight_definitions = {
+      				enable = true,
+      				clear_on_cursor_move = true
+    			},
+    			highlight_current_scope = { enable = true },
+    			smart_rename = {
+      				enable = true,
+      				keymaps = {
+        				smart_rename = "tr",
+      				},
+    			},
+  		},
+	}
 }
