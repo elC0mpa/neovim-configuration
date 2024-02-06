09df9fb6d7372c1cbd8dc9cadc2b555711207737
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2024-02-05 20:12:18 -0500

    Updated leap configuration

--- lua/plugins_config/leap.lua
+++ lua/plugins_config/leap.lua
@@ -1,6 +1,7 @@
 return {
 	'ggandor/leap.nvim',
+	dependencies = { 'tpope/vim-repeat' },
 	config = function()
-	require('leap').add_default_mappings()
+		require('leap').add_default_mappings()
 	end
 }
