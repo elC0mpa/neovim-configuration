0432d824cd76d6f6693011b5996a2a30515a0b97
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2024-06-23 19:06:14 -0400

    Fixed line blame gitsigns

--- lua/plugins_config/gitsigns.lua
+++ lua/plugins_config/gitsigns.lua
@@ -24,9 +24,7 @@ return {
 			delay = 500,
 			ignore_whitespace = false,
 		},
-		current_line_blame_formatter = {
-			relative_time = false
-		},
+		current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
 		sign_priority                = 6,
 		update_debounce              = 100,
 		status_formatter             = nil, -- Use default
