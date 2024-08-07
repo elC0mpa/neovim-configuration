1fa1405b5f2dd6ca2c7cd3d5dbbe39676b4e617a
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2024-08-07 00:22:06 -0400

    Using lsp instead of coc

---
+++ lua/plugins_config/mason-lspconfig.lua
@@ -0,0 +1,6 @@
+return {
+	"williamboman/mason-lspconfig.nvim",
+	opts = {
+		ensure_installed = { "dockerls", "gopls", "html", "css_variables", "css_ls", "cssmodules_ls", "tailwindcss", "tsserver", "eslint" }
+	}
+}
