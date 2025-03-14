ad857487527a0da472f09307c3c2f0c32b2aab91
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2025-03-14 14:50:19 +0400

    Updated terraform config

--- lua/plugins_config/lsp_core.lua
+++ lua/plugins_config/lsp_core.lua
@@ -13,6 +13,9 @@ return {
 		"neovim/nvim-lspconfig",
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			require 'lspconfig'.terraformls.setup {
+				capabilities = capabilities
+			}
 			require 'lspconfig'.gopls.setup {
 				capabilities = capabilities
 			}
