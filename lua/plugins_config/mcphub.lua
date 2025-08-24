20b772fc1f9db4245a0fe5d45f5a1b0dbf46f3aa
Jose Gabriel Companioni Benitez <53531665+elC0mpa@users.noreply.github.com>
2025-08-24 23:33:57 +0400

    Added avante config

---
+++ lua/plugins_config/mcphub.lua
@@ -0,0 +1,19 @@
+return {
+	"ravitemer/mcphub.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
+	config = function()
+		require("mcphub").setup({
+			extensions = {
+				copilotchat = {
+					enabled = true,
+					convert_tools_to_functions = true,
+					convert_resources_to_functions = true,
+					add_mcp_prefix = false,
+				},
+			},
+		})
+	end,
+}
